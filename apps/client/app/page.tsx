e9d217ed1c6762bf6e30b1ced58700ef909d7274
Tanishq TaNiShQ-000 <tanishq.jaiswal000@gmail.com>
2024-04-20 10:09:56 +0530

    adding recoil done

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+// import { PrismaClient } from "../../../packages/db";
+import { PrismaClient } from "@repo/db/client";
 
 export default function Page(): JSX.Element {
   return (
